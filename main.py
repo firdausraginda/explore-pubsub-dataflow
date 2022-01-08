d522bc123f0d5de43d83811677317e165f280453
firdausraginda <ragindafirdaus01@gmail.com>
2022-01-08 17:00:26 +0700

    create .gitignore for service account

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+input_file = 'gs://dummy-bucket-files/lorem.txt'
+output_path = 'gs://dummy-bucket-files/result.txt'
+
+beam_options = PipelineOptions(
+    runner='DataflowRunner',
+    project='another-dummy-project-337513',
+    job_name='dummy-job-1',
+    temp_location='gs://dummy-dataflow-temp/temp',
+    region='us'
+)
+
+with beam.Pipeline(options=beam_options) as p:
+    pass
