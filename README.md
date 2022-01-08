cd2c945837d01ff98466e982702095dfc333aacb
firdausraginda <ragindafirdaus01@gmail.com>
2022-01-08 23:04:26 +0700

    create dummy text folder in local; add notes in README.md; complete the main.py pipeline

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 # dependency
 * install apache-beam for gcp: `pipenv install 'apache-beam[gcp]'`
+
+# notes
+* `apache-beam` only works for python version 3.8.9 and below
+* using `'apache-beam[gcp]'`, will automatically create dataflow job when run the script
+* can create multiple job in dataflow with the same `job_name`
+* apache-beam runners: https://beam.apache.org/documentation/#available-runners
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from setup_gcp import setup_creds
+import re
+import json
 
-input_file = 'gs://dummy-bucket-files/lorem.txt'
+
+input_file = 'gs://dummy-bucket-files/dummy_text_1.txt'
 output_path = 'gs://dummy-bucket-files/result.txt'
 
 beam_options = PipelineOptions(
@@ -16,5 +19,45 @@ beam_options = PipelineOptions(
 # setup credential gcp
 setup_creds()
 
-with beam.Pipeline(options=beam_options) as p:
-    pass
+
+def print_data(data):
+    print(data)
+
+def to_json(data):
+    json_ob = {}
+
+    for item in data:
+        if item in json_ob:
+            json_ob[item] += 1
+        else:
+            json_ob[item] = 1
+
+    return json_ob
+
+
+# with beam.Pipeline(options=beam_options) as p:
+with beam.Pipeline() as p:
+    pre_processing = (
+        p
+        | 'read file' >> beam.io.ReadFromText(input_file)
+        | beam.Map(lambda words: words.lower())
+        | beam.Map(lambda words: re.sub(r'[^\w]', ' ', words))
+        | 'remove white spaces before and after string' >> beam.Map(lambda words: words.strip())
+        | 'remove multiple white spaces between string' >> beam.Map(lambda words: re.sub(r' +', ' ', words))
+        | 'split to list' >> beam.Map(lambda words: words.split(' '))
+        # | 'print result' >> beam.Map(print_data)
+    )
+
+    convert_to_json = (
+        pre_processing
+        | 'convert to json' >> beam.Map(to_json)
+        | 'serialize' >> beam.Map(lambda data: json.dumps(data))
+        # | 'print result json' >> beam.Map(print_data)
+    )
+
+    (
+        convert_to_json
+        | beam.io.WriteToText('result', 
+            file_name_suffix='.json'
+            )
+    )
