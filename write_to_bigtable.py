6260ed17a04012ce5c9e0cb9fc49a3fcbda2f51d
firdausraginda <ragindafirdaus01@gmail.com>
2022-01-16 14:50:46 +0700

    checkpoint write person hobbies data to bigtable

--- write_to_bigtable.py
+++ write_to_bigtable.py
@@ -3,35 +3,36 @@ from google.cloud import bigtable
 from setup_gcp import setup_creds
 
 
-def write_simple(project_id, instance_id, table_id):
+def bigtable_config():
+
+    project_id = 'another-dummy-project-337513'
+    instance_id = 'dummy-bt'
+    table_id = 'dummy-table-bt'
+
     client = bigtable.Client(project=project_id, admin=True)
     instance = client.instance(instance_id)
     table = instance.table(table_id)
 
+    return table
+
+
+def write_simple(table, row_key, column_family_id, data):
+
     timestamp = datetime.datetime.utcnow()
-    column_family_id = "stats_summary"
-
-    row_key = "phone#4c410523#20190501"
-
     row = table.direct_row(row_key)
-    row.set_cell(column_family_id, "connected_cell", 1, timestamp)
-    row.set_cell(column_family_id, "connected_wifi", 1, timestamp)
-    row.set_cell(column_family_id, "os_build", "PQ2A.190405.003", timestamp)
+
+    for item in data:
+        row.set_cell(column_family_id, item['person'], item['hobby'], timestamp)
 
     row.commit()
 
     print("Successfully wrote row {}.".format(row_key))
 
 
-def read_rows(project_id, instance_id, table_id):
-    client = bigtable.Client(project=project_id, admin=True)
-    instance = client.instance(instance_id)
-    table = instance.table(table_id)
+def read_rows(table, row_key, column_family_id, column_id):
 
-    row_key = "phone#4c410523#20190501"
     row = table.read_row(row_key.encode("utf-8"))
-    column_family_id = "stats_summary"
-    column_id = "connected_cell".encode("utf-8")
+    column_id = column_id.encode("utf-8")
     value = row.cells[column_family_id][column_id][0].value.decode("utf-8")
 
     print(value)
@@ -40,9 +41,18 @@ def read_rows(project_id, instance_id, table_id):
 # setup credential gcp
 setup_creds()
 
-project_id = 'another-dummy-project-337513'
-instance_id = 'dummy-bt'
-table_id = 'dummy-table-bt'
+# setup bigtable
+table = bigtable_config()
 
-# write_simple(project_id, instance_id, table_id)
-read_rows(project_id, instance_id, table_id)
+# define data
+row_key = 'hobbies_1'
+column_family_id = 'person_hobbies'
+column_id = 'person'
+person_hobbies = [
+    {'person': 'mike', 'hobby': 'swimming'},
+    {'person': 'jhon', 'hobby': 'dancing'},
+    {'person': 'melia', 'hobby': 'cooking'}
+]
+
+write_simple(table, row_key, column_family_id, person_hobbies)
+# read_rows(project_id, instance_id, table_id)
