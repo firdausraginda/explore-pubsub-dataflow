308bbd6c43cfb9c3b2223bc1c9d6d49b71164869
firdausraginda <ragindafirdaus01@gmail.com>
2022-01-16 17:45:42 +0700

    modify write & read bigtable

--- write_read_bigtable.py
+++ write_read_bigtable.py
@@ -6,8 +6,8 @@ from setup_gcp import setup_creds
 def bigtable_config():
 
     project_id = 'another-dummy-project-337513'
-    instance_id = 'dummy-bt'
-    table_id = 'dummy-table-bt'
+    instance_id = 'dummy-instance'
+    table_id = 'my-table'
 
     client = bigtable.Client(project=project_id, admin=True)
     instance = client.instance(instance_id)
@@ -29,13 +29,28 @@ def write_simple(table, row_key, column_family_id, data):
     print("Successfully wrote row {}.".format(row_key))
 
 
-def read_rows(table, row_key, column_family_id, column_id):
+def read_row(table, row_key, column_family_id, column_id):
 
     row = table.read_row(row_key.encode("utf-8"))
     column_id = column_id.encode("utf-8")
-    value = row.cells[column_family_id][column_id][0].value.decode("utf-8")
+    value = row.cells[column_family_id][column_id]
 
     print(value)
+    print(len(value))
+
+
+def read_cell_by_column_id(table, row_key, column_family_id, column_id):
+
+    row = table.read_row(row_key.encode("utf-8"))
+    column_id = column_id.encode("utf-8")
+    cols = row.cells[column_family_id][column_id]
+    
+    values = []
+    for col in cols:
+        temp_val = col.value.decode("utf-8")
+        values.append(temp_val)
+
+    print(column_id, values)
 
 
 # setup credential gcp
@@ -47,12 +62,12 @@ table = bigtable_config()
 # define data
 row_key = 'hobbies_1'
 column_family_id = 'person_hobbies'
-column_id = 'person'
 person_hobbies = [
     {'person': 'mike', 'hobby': 'swimming'},
     {'person': 'jhon', 'hobby': 'dancing'},
     {'person': 'melia', 'hobby': 'cooking'}
 ]
 
-write_simple(table, row_key, column_family_id, person_hobbies)
-# read_rows(project_id, instance_id, table_id)
+# write_simple(table, row_key, column_family_id, person_hobbies)
+read_row(table, 'hobbies_1', 'person_hobbies', 'mike')
+read_cell_by_column_id(table, 'hobbies_1', 'person_hobbies', 'mike')
